27fdff14409e3dab28d3f8e58cd69f3c5d491154
Alexander Adamovskiy <94919988+Alexander0k@users.noreply.github.com>
2022-08-01 13:27:51 +0300

    Завдання 2, виконав

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,16 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+for (let i = 0; i < ingredients.length; i += 1) {
+  const ingredient = ingredients[i]
+  const itemEl = document.createElement('li')
+  itemEl.textContent = ingredient
+  const ingredientsUl = document.querySelector('ul')
+  ingredientsUl.appendChild(itemEl)
+}
+
+
+
+
+
