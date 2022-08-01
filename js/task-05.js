2734866ba75f86160811bd43e76682b7313de5b8
Alexander Adamovskiy <94919988+Alexander0k@users.noreply.github.com>
2022-08-01 19:41:53 +0300

    Завдання 5, виконав

---
+++ js/task-05.js
@@ -0,0 +1,7 @@
+const refs = {
+    inputEl: document.querySelector("#name-input"),
+    nameInputEl: document.querySelector("#name-output"),
+}
+refs.inputEl.addEventListener("input", (event) => {
+    refs.nameInputEl.textContent = event.currentTarget.value
+})
