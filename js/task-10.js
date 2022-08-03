2065084ae44e6e41c3e92cf102efbd8c3359c781
Alexander Adamovskiy <94919988+Alexander0k@users.noreply.github.com>
2022-08-03 16:47:23 +0300

    завдання 10, виконав

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,33 @@
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+const refs = {
+  inputEl: document.querySelector('[type="number"]'),
+  createBtnEl: document.querySelector('[data-create]'),
+  destroyBtnEl: document.querySelector('[data-destroy]'),
+  boxesEl: document.querySelector('#boxes'),
+};
+
+const getRandomHexColor = function () {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
 }
+
+refs.createBtnEl.addEventListener('click', function createBoxes(amount) {
+  amount = refs.inputEl.value;
+
+  const arrNumbers = [];
+  for (let i = 0; i < amount; i += 1) {
+    const divBox = `<div>DivBox${i + 1}</div>`;
+    arrNumbers.push(divBox);
+  };
+
+  const boxesMarkup = arrNumbers
+    .map((arrNumber) => arrNumber)
+    .join("");
+
+  refs.boxesEl.innerHTML = boxesMarkup;
+});
+
+refs.destroyBtnEl.addEventListener('click', function destroyBoxes() {
+  refs.boxesEl.innerHTML = "";
+  refs.inputEl.value = "";
+});
